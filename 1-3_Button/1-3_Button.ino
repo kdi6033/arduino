e79c31e6c48114e473cca0e005459469c62f52b6
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2018-04-11 22:01:47 +0900

    소스 업로드

---
+++ 1-3_Button/1-3_Button.ino
@@ -0,0 +1,27 @@
+// D4번 핀에 따라 Led가 on off 된다.
+const int buttonPin = 3;     // the number of the pushbutton pin D0 핀
+const int ledPin =  2;      // the number of the LED pin
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
