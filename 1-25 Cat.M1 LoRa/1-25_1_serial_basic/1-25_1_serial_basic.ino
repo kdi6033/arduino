12dcea23955cdd7cad93ca6fb559115034ce3f45
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2019-03-18 15:35:18 +0900

    시리얼 통신 프로그램

---
+++ 1-25 Cat.M1 LoRa/1-25_1_serial_basic/1-25_1_serial_basic.ino
@@ -0,0 +1,37 @@
+unsigned long previousMillis = 0;  
+const long interval = 3000; //단위 ms
+void setup() {
+  // initialize serial:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial.println();
+  Serial.println("Test Strat");
+}
+
+void loop() {
+  serialEvent();
+  //SendText();
+}
+
+void SendText() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    Serial.println("AT");
+    Serial1.write("AT");
+    Serial1.write(0x0d);
+    Serial1.write(0x0a);
+  }
+}
+
+void serialEvent() {
+  if(Serial.available() == false)
+    return;
+  String inChar;
+  while (Serial.available()) {
+    // get the new byte:
+    inChar += (char)Serial.read();
+  }
+  Serial.println(inChar);
+  Serial1.println(inChar);
+}
