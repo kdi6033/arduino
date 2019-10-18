de73503af437e908ca7e119ab0b205df43be032a
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2019-10-18 11:38:33 +0900

    1-26-4 정리

---
+++ 1-26-4-1_Internet Communication Return Home/HelloServer/HelloServer.ino
@@ -0,0 +1,94 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "i2r";
+const char* password = "00000000";
+
+ESP8266WebServer server(80);
+
+const int led = 2;
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 1);
+  Serial.begin(9600);
+  
+  connectWifi();
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void connectWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop(void) {
+  server.handleClient();
+}
+
+void handleRoot() {
+  String s; 
+  s="<meta name='viewport' content='width=device-width, initial-scale=1.0'/>";
+  //s=s+"<meta http-equiv='refresh' content='5'/>";
+  s=s+"<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />";
+  s=s+"<h1>김동일 웹서버 i2r</h1>";
+  s=s+"<a href=\"on\"><button style='background-color:Lime; color:blue;'>켜짐 on</button></a>";
+  s=s+"&nbsp&nbsp";
+  s=s+"<a href=\"off\"><button style='background-color:Gray; color:blue;'>꺼짐 off</button></a>";
+  server.send(200, "text/html", s);
+}
+
+void handleOn() {
+  digitalWrite(led, 0);
+  GoHome();
+}
+
+void handleOff() {
+  digitalWrite(led, 1);
+  GoHome();
+}
+
+void GoHome() {
+  IPAddress ip = WiFi.localIP();
+  String s,ipS;
+  ipS=String(ip[0])+"."+String(ip[1])+"."+String(ip[2])+"."+String(ip[3]);
+    s="<meta http-equiv='refresh' content=\"0;url='http://"+ipS+"/'\">";
+  server.send(200, "text/html", s);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
