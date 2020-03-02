83d444f5b59b501aa022b2414d537227053d0cc5
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2020-03-02 15:43:49 +0900

    Facrory Reset

---
+++ 1-6-5 WiFi Factory Reset/ServerAP/ServerAP.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+/* Set these to your desired credentials. */
+const char *softAP_ssid = "i2r";
+const char *softAP_password = "";
+/* Soft AP network parameters */
+IPAddress apIP(192, 168, 4, 1);
+IPAddress netMsk(255, 255, 255, 0);
+
+ESP8266WebServer server(80);
+
+const int led = 2;
+
+void setup(void) {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 1);
+  
+  setupAp();
+
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void setupAp() {
+  Serial.println(softAP_ssid);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP(softAP_ssid, softAP_password);
+  delay(500); // Without delay I've seen the IP address blank
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void loop(void) {
+  server.handleClient();
+}
+
+void handleRoot() {
+  String s; 
+  s="<meta name='viewport' content='width=device-width, initial-scale=1.0'/>";
+  //s=s+"<meta http-equiv='refresh' content='5'/>";
+  s=s+"<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />";
+  s=s+"<h1>김동일 웹서버 i2r</h1>";
+  s=s+"<a href=\"on\"><button style='background-color:Lime; color:blue;'>켜짐 on</button></a>";
+  s=s+"&nbsp&nbsp";
+  s=s+"<a href=\"off\"><button style='background-color:Gray; color:blue;'>꺼짐 off</button></a>";
+  server.send(200, "text/html", s);
+}
+
+void handleOn() {
+  digitalWrite(led, 0);
+  GoHome();
+}
+
+void handleOff() {
+  digitalWrite(led, 1);
+  GoHome();
+}
+
+void GoHome() {
+  //IPAddress ip = WiFi.localIP();
+  IPAddress ip = apIP;
+  String s,ipS;
+  ipS=String(ip[0])+"."+String(ip[1])+"."+String(ip[2])+"."+String(ip[3]);
+    s="<meta http-equiv='refresh' content=\"0;url='http://"+ipS+"/'\">";
+  server.send(200, "text/html", s);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
