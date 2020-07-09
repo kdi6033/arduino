7aaa4d501ad25468281f5f005411b561516f954b
kdi6033 <kdi6033@gmail.com>
2020-07-09 22:39:52 +0900

    AWS mqtt Basic 프로그램

---
+++ 1-101_5 ArduinoWebMqttAWS/ServerMqttAWS/handleHttp.ino
@@ -0,0 +1,126 @@
+/** Handle root or redirect to captive portal */
+
+void handleRoot() {
+  String s; 
+  s="<meta name='viewport' content='width=device-width, initial-scale=1.0'/>";
+  //s=s+"<meta http-equiv='refresh' content='5'/>";
+  s=s+"<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />";
+  s=s+"<h1>김동일 웹서버 i2r.link</h1>";
+  s=s+"<a href=\"on\"><button style='background-color:Lime; color:blue;'>켜짐 on</button></a>";
+  s=s+"&nbsp&nbsp";
+  s=s+"<a href=\"off\"><button style='background-color:Gray; color:blue;'>꺼짐 off</button></a>";
+  if(bootMode==1)
+    s=s+"<br><br>AP & IP :&emsp;"+String(softAP_ssid)+"&emsp;<a href='http://"+WiFi.softAPIP().toString()+"'/>"+WiFi.softAPIP().toString()+"</a>";
+  else
+    s=s+"<br><br>AP & IP :&emsp;"+String(softAP_ssid)+"&emsp;<a href='http://"+WiFi.localIP().toString()+"'/>"+WiFi.localIP().toString()+"</a>";
+  s=s+"<p><a href='/wifi'>네트웍공유기를 바꾸려면 누르세요.</a></p>";
+
+  server.send(200, "text/html", s);
+}
+
+
+void handleOn() {
+  digitalWrite(led, 0);
+  GoHome();
+}
+
+void handleOff() {
+  digitalWrite(led, 1);
+  GoHome();
+}
+
+void GoHome() {
+  String s,ipS;
+  IPAddress ip;
+  if(bootMode==1)
+    ip = apIP;
+  else
+    ip = WiFi.localIP();
+  
+  ipS=String(ip[0])+"."+String(ip[1])+"."+String(ip[2])+"."+String(ip[3]);
+    s="<meta http-equiv='refresh' content=\"0;url='http://"+ipS+"/'\">";
+  server.send(200, "text/html", s);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void handleWifi() {
+  String s; 
+  String s1= String(ssid);
+  s="<meta name=\"viewport\" content=\"width=device-width, user-scalable=no\", meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />";
+  //s=s+"<meta http-equiv='refresh' content='5'/>";
+  s=s+"<h1>Wifi 사양</h1>";
+  if (server.client().localIP() == apIP) {
+    Serial.println(String(softAP_ssid));
+    //s=s+String("<p>연결된 AP: ") + sAP_ssid + "</p>";
+    s=s+String("<p>연결된 AP: 192.168.4.1") + "</p>";
+  } else {
+    s=s+"<p>연결된 와이파이 " + String(ssid) + "</p>";
+  }
+  s=s+"<table><tr><th align='left'>SoftAP 사양</th></tr>";
+  s=s+"<tr><td>SSID " + String(softAP_ssid) + "</td></tr>";
+  s=s+"<tr><td>IP   " + toStringIp(WiFi.softAPIP()) + "</td></tr>"+"</table>";
+  s=s+"<br /><table><tr><th align='left'>WLAN 사양</th></tr>";
+  s=s+"<tr><td>SSID " + String(ssid) + "</td></tr>";
+  s=s+"<tr><td>IP   " + toStringIp(WiFi.localIP()) + "</td></tr>"+"</table>";
+  
+  s=s+"<br /><table><tr><th align='left'>검색된 와이파이 (SSID)</th></tr>";
+  Serial.println("scan start");
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n > 0) {
+    for (int i = 0; i < n; i++) 
+      s=s+"\r\n<tr><td>" + WiFi.SSID(i) + "</td></tr>";
+      //s=s+"\r\n<tr><td>SSID " + WiFi.SSID(i) + String((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":" *") + " (" + WiFi.RSSI(i) + ")</td></tr>";
+  } 
+  else 
+    s=s+"<tr><td>No WLAN found</td></tr>";
+  s=s+"</table>";
+  s=s+"<p><a href='/wifi'>와이파이가 없으면 다시 검색하세요.</a></p>";
+  
+  s=s+"<form method='POST' action='wifisave'><h4>연결하려는 와이파이 입력</h4>"
+    +"와이파이 이름 <input type='text' value='"+ssid+"' name='n'/>"
+    +"<br/><br/>비밀번호     <input type='password' value='"+password+"' name='p'/>"
+    +"<br/><br/><input type='submit' value='      저    장      '/></form>"
+    +"<p><a href='/'>메인 홈페이지로 가기</a>.</p>";
+  server.send(200, "text/html", s);
+}
+
+/** Handle the WLAN save form and redirect to WLAN config page again */
+void handleWifiSave() {
+  Serial.println("wifi save");
+  server.arg("n").toCharArray(ssid, sizeof(ssid) - 1);
+  server.arg("p").toCharArray(password, sizeof(password) - 1);
+  server.sendHeader("Location", "wifi", true);
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send ( 302, "text/plain", "");  // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.client().stop(); // Stop is needed because we sent no content length
+  saveCredentials();
+  bConnect = strlen(ssid) > 0; // Request WLAN connect with new credentials if there is a SSID
+  Serial.println(WiFi.waitForConnectResult());
+  Serial.println(ssid);
+  Serial.println(password);
+  Serial.println("Reset");
+  ESP.reset();
+}
+
+void handleScan() {
+  String s;
+  s="{\"mac\":\""+sMac+"\",\"ip\":\""+WiFi.localIP().toString()+"\"}";
+  server.send(200, "text/html", s);
+}
