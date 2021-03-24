6e126110f508cacca6ea4c730a7135a4b5fb6b05
kdi6033 <kdi6033@gmail.com>
2021-03-24 15:15:40 +0900

    mqtt D1 mini Ticker.h 추가하여 3초마다 메시지 보냄 publish, 스마트폰 mqtt 서버

---
+++ 1-16-2-1 mqtt_esp8266/mqtt_esp8266/mqtt_esp8266.ino
@@ -0,0 +1,108 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Ticker.h>
+Ticker ticker;
+
+#define TRIGGER_PIN 0 // trigger pin 0(D3) 2(D4)
+
+// 아래의 6개설정은 사용자 환경에 맞게 수정하세요.
+const char* ssid = "i2r"; // 와이파이 AP, 또는 스마트폰의 핫스판 이름
+const char* password = "00000000";  // 와이파이 AP, 또는 스마트폰의 핫스판 이름
+//const char* mqtt_server = "broker.mqtt-dashboard.com"; //브로커 주소
+const char* mqtt_server = "192.168.0.2"; //브로커 주소
+const char* outTopic = "/kdi/outTopic"; // 이름이 중복되지 않게 설정 기록
+const char* inTopic = "/kdi/inTopic"; // 이름이 중복되지 않게 설정 기록
+const char* clientName = "";  // setup 함수에서 자동생성
+String sChipID;
+char cChipID[20];
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[100];
+int count=0;
+
+void tick()
+{
+  //toggle state
+  count++;
+  snprintf (msg, 75, "count #%d", count);
+  client.publish(outTopic, msg);
+  Serial.println(count);
+}
+
+void setup() {
+  Serial.begin(9600);
+  setup_wifi();
+  ticker.attach(3, tick);  //0.1 초도 가능
+  //ticker.detach();
+
+  //이름 자동으로 생성
+  sChipID=String(ESP.getChipId(),HEX);
+  sChipID.toCharArray(cChipID,sChipID.length()+1);
+  clientName=&cChipID[0];
+  Serial.println(clientName);
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// 통신에서 문자가 들어오면 이 함수의 payload 배열에 저장된다.
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+// mqtt 통신에 지속적으로 접속한다.
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientName)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(outTopic, "Reconnected");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
