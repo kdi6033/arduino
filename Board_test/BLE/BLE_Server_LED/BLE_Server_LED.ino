ffc25278cd8f7dc161c8a3087db8486d73012054
Dongil Kim <37902752+kdi6033@users.noreply.github.com>
2025-02-17 22:30:51 +0900

    Update BLE_Server_LED.ino

--- Board_test/BLE/BLE_Server_LED/BLE_Server_LED.ino
+++ Board_test/BLE/BLE_Server_LED/BLE_Server_LED.ino
@@ -2,109 +2,116 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#define LED_PIN 47      // LED가 연결된 핀 번호
+
+#define LED_PIN 47  // LED가 연결된 핀 번호
 
 BLEServer *pServer = NULL;
-BLECharacteristic * pTxCharacteristic;
+BLECharacteristic *pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint8_t txValue = 0;
-uint32_t value = 0;
+uint32_t count = 0;  // count 변수 선언
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) {
+        deviceConnected = true;
     };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
+    void onDisconnect(BLEServer *pServer) {
+        deviceConnected = false;
     }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
+class MyCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-        String rxValue = String(pCharacteristic->getValue().c_str()); // 변환
+        String rxValue = String((char*)pCharacteristic->getValue().c_str()); // 변환
+
+        rxValue.trim();  // 개행 문자 및 공백 제거
 
         if (rxValue.length() > 0) {
-            Serial.println("*********");
             Serial.print("Received Value: ");
-            Serial.println(rxValue); // 문자열 출력
+            Serial.println(rxValue);
 
-            if(rxValue == "Turn on"){
+            if (rxValue == "Turn on") {
                 digitalWrite(LED_PIN, HIGH);
-            }
-            else if(rxValue == "Turn off"){
+            } else if (rxValue == "Turn off") {
                 digitalWrite(LED_PIN, LOW);
             }
-            Serial.println("*********");
+
+            // count 증가 후 응답 메시지 생성
+            count++;
+            String response = "Server: " + rxValue + " " + String(count);
+
+            // 클라이언트에게 응답 전송
+            pTxCharacteristic->setValue(response.c_str());
+            pTxCharacteristic->notify();
+
+            //Serial.println("Sent Response: " + response);
         }
     }
 };
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN,OUTPUT);
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
 
-  // Create the BLE Device
-  BLEDevice::init("LightTalk");
+    // Create the BLE Device
+    BLEDevice::init("LightTalk");
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
-  pTxCharacteristic = pService->createCharacteristic(
-										CHARACTERISTIC_UUID_TX,
-										BLECharacteristic::PROPERTY_NOTIFY
-									);
-                      
-  pTxCharacteristic->addDescriptor(new BLE2902());
+    // Create a BLE Characteristic
+    pTxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_TX,
+        BLECharacteristic::PROPERTY_NOTIFY
+    );
 
-  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
-											 CHARACTERISTIC_UUID_RX,
-											BLECharacteristic::PROPERTY_WRITE
-										);
+    pTxCharacteristic->addDescriptor(new BLE2902());
 
-  pRxCharacteristic->setCallbacks(new MyCallbacks());
+    BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_RX,
+        BLECharacteristic::PROPERTY_WRITE
+    );
 
-  // Start the service
-  pService->start();
+    pRxCharacteristic->setCallbacks(new MyCallbacks());
 
-  pServer->getAdvertising()->addServiceUUID(SERVICE_UUID);
+    // Start the service
+    pService->start();
 
-  // Start advertising
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
+    pServer->getAdvertising()->addServiceUUID(SERVICE_UUID);
+
+    // Start advertising
+    pServer->getAdvertising()->start();
+    Serial.println("Waiting for a client connection to notify...");
 }
 
 void loop() {
-  if (deviceConnected) {
-    String inputString = "test";
-    //pTxCharacteristic->setValue(&txValue, 1);
-    pTxCharacteristic->setValue(inputString.c_str());
-    pTxCharacteristic->notify();
-    //txValue++;
-		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
-	}
+    // 연결된 경우
+    if (deviceConnected) {
+        // 필요할 경우 추가 기능 구현 가능
+    }
 
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected) {
-      delay(500); // give the bluetooth stack the chance to get things ready
-      pServer->startAdvertising(); // restart advertising
-      Serial.println("start advertising");
-      oldDeviceConnected = deviceConnected;
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected) {
-  // do stuff here on connecting
-      oldDeviceConnected = deviceConnected;
-  }
-  delay(1000);
+    // 연결이 끊어진 경우 재광고 시작
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500);  // Bluetooth 스택을 준비할 시간 제공
+        pServer->startAdvertising();
+        Serial.println("Restarting advertising...");
+        oldDeviceConnected = deviceConnected;
+    }
+
+    // 새로운 연결이 이루어진 경우
+    if (deviceConnected && !oldDeviceConnected) {
+        oldDeviceConnected = deviceConnected;
+    }
+
+    delay(500);
 }
+
