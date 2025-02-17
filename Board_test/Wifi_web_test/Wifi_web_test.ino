80a7117ac57e399c058c2476105deee8c0ffa9a7
Dongil Kim <37902752+kdi6033@users.noreply.github.com>
2025-02-17 14:15:32 +0900

    Update Wifi_web_test.ino

--- Board_test/Wifi_web_test/Wifi_web_test.ino
+++ Board_test/Wifi_web_test/Wifi_web_test.ino
@@ -3,87 +3,84 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-const char* ssid = "사용하시는 와이파이 이름";     // 사용하실 와이파이 정보를 꼭 수정해서 입력해주세요! 자동연결 됩니다.
-const char* password = "와이파이 비밀번호";
-unsigned long previousMillis = 0;     
-const long interval = 2000; 
-int counting;
-WebServer server(80);
+const char* ssid = "i2r";          // Wi-Fi SSID
+const char* password = "00000000"; // Wi-Fi 비밀번호
 
-//const int led = 13;
+WebServer server(80); // HTTP 서버 포트 80번 사용
+const int ledPin = 47; // GPIO 47번 핀 사용
+bool ledState = false; // LED 상태 저장
 
+// 웹페이지 (HTML + JavaScript)
+const char MAIN_page[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ESP32-S3 Web Server</title>
+    <script>
+        function sendCommand(state) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/toggle?state=" + state, true);
+            xhr.send();
+        }
+    </script>
+</head>
+<body>
+    <h1>ESP32-S3 GPIO 47 Control</h1>
+    <button onclick="sendCommand('on')">ON</button>
+    <button onclick="sendCommand('off')">OFF</button>
+</body>
+</html>
+)rawliteral";
+
+// "/" 경로 요청 시 HTML 페이지 전송
 void handleRoot() {
-  //digitalWrite(led, 1);
-  //server.send(200, "text/plain", "test!");
-  //digitalWrite(led, 0);
+    server.send(200, "text/html", MAIN_page);
 }
 
-void handleNotFound() {
-  //digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  //digitalWrite(led, 0);
-}
-
-void setup(void) {
-  //pinMode(led, OUTPUT);
-  //digitalWrite(led, 0);
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp32")) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", handleRoot);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
-}
-
-void tick() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    counting++;
-    if(counting==1000){
-      counting=0;
+// GPIO 47 ON/OFF 제어
+void handleToggle() {
+    if (server.hasArg("state")) {
+        String state = server.arg("state");
+        if (state == "on") {
+            digitalWrite(ledPin, HIGH);
+            ledState = true;
+        } else if (state == "off") {
+            digitalWrite(ledPin, LOW);
+            ledState = false;
+        }
     }
-  }
+    server.send(200, "text/plain", ledState ? "ON" : "OFF");
 }
 
-void loop(void) {
-  tick();
-  server.handleClient();
-  server.send(200, "text/plain", String(counting));
-  delay(2);//allow the cpu to switch to other tasks
+void setup() {
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to WiFi...");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    pinMode(ledPin, OUTPUT); // GPIO 47번 핀 출력으로 설정
+    digitalWrite(ledPin, LOW); // 초기 상태 OFF
+
+    // MDNS 설정
+    if (MDNS.begin("esp32")) {
+        Serial.println("MDNS responder started");
+    }
+
+    // HTTP 라우트 설정
+    server.on("/", handleRoot);
+    server.on("/toggle", handleToggle);
+    server.begin();
+    Serial.println("HTTP server started");
 }
+
+void loop() {
+    server.handleClient();
+}
+
